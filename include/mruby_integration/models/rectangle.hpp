dc21d1ffee265890903de8819b090bc8af478fb8
mooff <mooff@awful.cooking>
2023-04-28 21:01:53 +0100

    Add setup_Rectangle() so Rectangle's can be initialized outside of rectangle.cpp

--- include/mruby_integration/models/rectangle.hpp
+++ include/mruby_integration/models/rectangle.hpp
@@ -4,4 +4,6 @@
 
 extern RClass *Rectangle_class;
 
+void setup_Rectangle(mrb_state*, mrb_value, Rectangle*, float, float, float, float);
+
 void append_models_Rectangle(mrb_state*);
--- src/mruby_integration/models/rectangle.cpp
+++ src/mruby_integration/models/rectangle.cpp
@@ -10,41 +10,55 @@
 
 struct RClass *Rectangle_class;
 
-mrb_value mrb_Rectangle_initialize(mrb_state *mrb, mrb_value self) {
+void setup_Rectangle(mrb_state *mrb, mrb_value object, Rectangle *rectangle, float x, float y, float width, float height)
+{
+  ivar_attr_float(mrb, object, rectangle->x, x);
+  ivar_attr_float(mrb, object, rectangle->y, y);
+  ivar_attr_float(mrb, object, rectangle->width, width);
+  ivar_attr_float(mrb, object, rectangle->height, height);
+}
+
+mrb_value mrb_Rectangle_initialize(mrb_state *mrb, mrb_value self)
+{
   mrb_float x, y, width, height;
   mrb_get_args(mrb, "ffff", &x, &y, &width, &height);
 
   Rectangle *rectangle = (struct Rectangle *)DATA_PTR(self);
-  if (rectangle) { mrb_free(mrb, rectangle); }
+  if (rectangle)
+  {
+    mrb_free(mrb, rectangle);
+  }
   mrb_data_init(self, nullptr, &Rectangle_type);
   rectangle = (Rectangle *)malloc(sizeof(Rectangle));
 
-  ivar_attr_float(mrb, self, rectangle->x, x);
-  ivar_attr_float(mrb, self, rectangle->y, y);
-  ivar_attr_float(mrb, self, rectangle->width, width);
-  ivar_attr_float(mrb, self, rectangle->height, height);
+  setup_Rectangle(mrb, self, rectangle, x, y, width, height);
 
   mrb_data_init(self, rectangle, &Rectangle_type);
   return self;
 }
 
-mrb_value mrb_Rectangle_set_x(mrb_state *mrb, mrb_value self) {
+mrb_value mrb_Rectangle_set_x(mrb_state *mrb, mrb_value self)
+{
   attr_setter_float(mrb, self, Rectangle_type, Rectangle, x, x);
 }
 
-mrb_value mrb_Rectangle_set_y(mrb_state *mrb, mrb_value self) {
+mrb_value mrb_Rectangle_set_y(mrb_state *mrb, mrb_value self)
+{
   attr_setter_float(mrb, self, Rectangle_type, Rectangle, y, y);
 }
 
-mrb_value mrb_Rectangle_set_width(mrb_state *mrb, mrb_value self) {
+mrb_value mrb_Rectangle_set_width(mrb_state *mrb, mrb_value self)
+{
   attr_setter_float(mrb, self, Rectangle_type, Rectangle, width, width);
 }
 
-mrb_value mrb_Rectangle_set_height(mrb_state *mrb, mrb_value self) {
+mrb_value mrb_Rectangle_set_height(mrb_state *mrb, mrb_value self)
+{
   attr_setter_float(mrb, self, Rectangle_type, Rectangle, height, height);
 }
 
-void append_models_Rectangle(mrb_state *mrb) {
+void append_models_Rectangle(mrb_state *mrb)
+{
   Rectangle_class = mrb_define_class(mrb, "Rectangle", mrb->object_class);
   MRB_SET_INSTANCE_TT(Rectangle_class, MRB_TT_DATA);
   mrb_define_method(mrb, Rectangle_class, "initialize", mrb_Rectangle_initialize, MRB_ARGS_REQ(4));
