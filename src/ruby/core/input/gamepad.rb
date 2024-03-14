baefe6c8975659c1f1ff70c3801b88add75629ca
Chadowo <83732118+Chadowo@users.noreply.github.com>
2024-03-14 13:56:33 -0300

    Fix grammar in Ruby core input documentation files Mostly added periods

--- src/ruby/core/input/gamepad.rb
+++ src/ruby/core/input/gamepad.rb
@@ -1,60 +1,60 @@
-# Unknown gamepad button
+# Unknown gamepad button.
 GAMEPAD_BUTTON_UNKNOWN = 0
 
-# Dpad up
+# D-pad up.
 GAMEPAD_BUTTON_LEFT_FACE_UP = 1
-# Dpad right
+# D-pad right.
 GAMEPAD_BUTTON_LEFT_FACE_RIGHT = 2
-# Dpad down
+# D-Pad down.
 GAMEPAD_BUTTON_LEFT_FACE_DOWN = 3
-# Dpad left
+# D-pad left.
 GAMEPAD_BUTTON_LEFT_FACE_LEFT = 4
 
-# XBOX: Y
-# PS3: Triangle
+# XBOX: Y.
+# PS3: Triangle.
 GAMEPAD_BUTTON_RIGHT_FACE_UP = 5
-# XBOX: X
-# PS3: Square
+# XBOX: X.
+# PS3: Square.
 GAMEPAD_BUTTON_RIGHT_FACE_RIGHT = 6
-# XBOX: A
-# PS3: Cross
+# XBOX: A.
+# PS3: Cross.
 GAMEPAD_BUTTON_RIGHT_FACE_DOWN = 7
-# XBOX: B
-# PS3: Circle
+# XBOX: B.
+# PS3: Circle.
 GAMEPAD_BUTTON_RIGHT_FACE_LEFT = 8
 
-# Left trigger 1
+# Left trigger 1.
 GAMEPAD_BUTTON_LEFT_TRIGGER_1 = 9
-# Left trigger 2
+# Left trigger 2.
 GAMEPAD_BUTTON_LEFT_TRIGGER_2 = 10
-# Right trigger 1
+# Right trigger 1.
 GAMEPAD_BUTTON_RIGHT_TRIGGER_1 = 11
-# Right trigger 2
+# Right trigger 2.
 GAMEPAD_BUTTON_RIGHT_TRIGGER_2 = 12
 
-# PS3: Select
+# PS3: Select.
 GAMEPAD_BUTTON_MIDDLE_LEFT = 13
-# Ps Button/XBOX Button
+# PS Button/XBOX Button.
 GAMEPAD_BUTTON_MIDDLE = 14
-# PS3: Start
+# PS3: Start.
 GAMEPAD_BUTTON_MIDDLE_RIGHT = 15
 
-# Left thumbstick pressed
+# Left thumbstick pressed.
 GAMEPAD_BUTTON_LEFT_THUMB = 16
-# Right thumbstick pressed
+# Right thumbstick pressed.
 GAMEPAD_BUTTON_RIGHT_THUM = 17
 
-# Left thumbstick axis X
+# Left thumbstick axis X.
 GAMEPAD_AXIS_LEFT_X = 0
-# Left thumbstick axis Y
+# Left thumbstick axis Y.
 GAMEPAD_AXIS_LEFT_Y = 1
 
-# Right thumbstick axis X
+# Right thumbstick axis X.
 GAMEPAD_AXIS_RIGHT_X = 2
-# Right thumbstick axis Y
+# Right thumbstick axis Y.
 GAMEPAD_AXIS_RIGHT_Y = 3
 
-# Pressure level for left trigger [1..-1] (pressure-level)
+# Pressure level for left trigger [1..-1] (pressure-level).
 GAMEPAD_AXIS_LEFT_TRIGGER = 4
-# Pressure level for right trigger [1..-1] (pressure-level)
+# Pressure level for right trigger [1..-1] (pressure-level).
 GAMEPAD_AXIS_RIGHT_TRIGGER = 5
--- src/ruby/core/input/mouse.rb
+++ src/ruby/core/input/mouse.rb
@@ -1,29 +1,29 @@
-# Left mouse button
+# Left mouse button.
 MOUSE_LEFT_BUTTON = 0
-# Right mouse button
+# Right mouse button.
 MOUSE_RIGHT_BUTTON = 1
-# Middle mouse button (scrollwheel)
+# Middle mouse button (scroll wheel).
 MOUSE_MIDDLE_BUTTON = 2
 
-# Mouse cursor default
+# Mouse cursor default.
 MOUSE_CURSOR_DEFAULT = 0
-# Mouse cursor arrow
+# Mouse cursor arrow.
 MOUSE_CURSOR_ARROW = 1
-# Mouse cursor ibeam
+# Mouse cursor I-Beam.
 MOUSE_CURSOR_IBEAM = 2
-# Mouse cursor crosshair
+# Mouse cursor crosshair.
 MOUSE_CURSOR_CROSSHAIR = 3
-# Mouse cursor pointing_hand
+# Mouse cursor pointing hand.
 MOUSE_CURSOR_POINTING_HAND = 4
-# Mouse cursor horizontal resize/move
+# Mouse cursor horizontal resize/move.
 MOUSE_CURSOR_RESIZE_EW = 5
-# Mouse cursor vertical resize/move
+# Mouse cursor vertical resize/move.
 MOUSE_CURSOR_RESIZE_NS = 6
-# Mouse cursor top-left to bottom-right diagonal resize/move
+# Mouse cursor top-left to bottom-right diagonal resize/move.
 MOUSE_CURSOR_RESIZE_NWSE = 7
-# Mouse cursor top-right to bottom-left diagonal resize/move
+# Mouse cursor top-right to bottom-left diagonal resize/move.
 MOUSE_CURSOR_RESIZE_NESW = 8
-# Mouse cursor omni-directional resize/move
+# Mouse cursor omni-directional resize/move.
 MOUSE_CURSOR_RESIZE_ALL = 9
-# Mouse cursor not allowed
+# Mouse cursor not allowed.
 MOUSE_CURSOR_NOT_ALLOWED = 10
