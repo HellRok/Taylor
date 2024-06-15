3b68ae895ea4020108de9b8adf8b3cb018f2908e
Sean Earle <sean.r.earle@gmail.com>
2024-06-15 14:40:05 +0930

    Add Cursor.enable, Cursor.disable, and Cursor.disabled?

--- src/mruby_integration/core/cursor.cpp
+++ src/mruby_integration/core/cursor.cpp
@@ -2,20 +2,6 @@
 #include "raylib.h"
 
 auto
-mrb_enable_cursor(mrb_state*, mrb_value) -> mrb_value
-{
-  EnableCursor();
-  return mrb_nil_value();
-}
-
-auto
-mrb_disable_cursor(mrb_state*, mrb_value) -> mrb_value
-{
-  DisableCursor();
-  return mrb_nil_value();
-}
-
-auto
 mrb_cursor_on_screen(mrb_state*, mrb_value) -> mrb_value
 {
   return mrb_bool_value(IsCursorOnScreen());
@@ -24,16 +10,6 @@ mrb_cursor_on_screen(mrb_state*, mrb_value) -> mrb_value
 void
 append_core_cursor(mrb_state* mrb)
 {
-  mrb_define_method(mrb,
-                    mrb->kernel_module,
-                    "enable_cursor",
-                    mrb_enable_cursor,
-                    MRB_ARGS_NONE());
-  mrb_define_method(mrb,
-                    mrb->kernel_module,
-                    "disable_cursor",
-                    mrb_disable_cursor,
-                    MRB_ARGS_NONE());
   mrb_define_method(mrb,
                     mrb->kernel_module,
                     "cursor_on_screen?",
--- test/models/cursor_test.rb
+++ test/models/cursor_test.rb
@@ -28,6 +28,34 @@ class Test
       ensure
         Cursor.show
       end
+
+      def test_enable
+        assert_false Cursor.disabled?
+        Cursor.disable
+        assert_true Cursor.disabled?
+        Cursor.enable
+        assert_false Cursor.disabled?
+      ensure
+        Cursor.enable
+      end
+
+      def test_disable
+        assert_false Cursor.disabled?
+        Cursor.disable
+        assert_true Cursor.disabled?
+      ensure
+        Cursor.enable
+      end
+
+      def test_disabled?
+        assert_false Cursor.disabled?
+        Cursor.disable
+        assert_true Cursor.disabled?
+        Cursor.enable
+        assert_false Cursor.disabled?
+      ensure
+        Cursor.enable
+      end
     end
   end
 end
