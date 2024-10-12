6f9ed2ea0fdc3d0e3e87a0b2bcfc538ce872a14c
Sean Earle <sean.r.earle@gmail.com>
2024-10-13 09:49:49 +1030

    Add the documentation patch by @Chadowo

--- mrb_doc/core/drawing.rb
+++ mrb_doc/core/drawing.rb
@@ -76,3 +76,20 @@ def end_scissor_mode
   # src/mruby_integration/core/drawing.cpp
   nil
 end
+
+# All draw calls within will be drawn using the specified blend mode.
+# @param mode [Integer]
+# @return [nil]
+def begin_blend_mode(mode)
+  # mrb_begin_blend_mode
+  # src/mruby_integration/core/drawing.cpp
+  nil
+end
+
+# End blend mode.
+# @return [nil]
+def end_blend_mode
+  # mrb_end_blend_mode
+  # src/mruby_integration/core/drawing.cpp
+  nil
+end
