f7ebf17c65347bffe2db2f15b04769c66f152d6c
Sean Earle <sean.r.earle@gmail.com>
2025-05-11 18:35:04 +0930

    Add platform tests

--- test/platform.rb
+++
@@ -1,9 +0,0 @@
-class Test
-  class Platform_Test < MTest::Unit::TestCaseWithAnalytics
-    if ENV["BUILDKITE_BUILD_ID"]
-      def test_released?
-        assert_true released?
-      end
-    end
-  end
-end
