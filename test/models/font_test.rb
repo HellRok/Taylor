46b50f9344e18b6a0bac22cccedc4d38bed8761f
Sean Earle <sean.r.earle@gmail.com>
2025-06-01 10:30:56 +0930

    Finish off the Font specs I missed

--- test/models/font_test.rb
+++ test/models/font_test.rb
@@ -17,18 +17,6 @@ class Test
         assert_equal 4, font.glyph_padding
       end
 
-      def test_unload
-        Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 1, glyph_count: 2, glyph_padding: 3))
-
-        font = Font.new("./assets/tiny.ttf")
-        Taylor::Raylib.reset_calls
-
-        font.unload
-        assert_called [
-          "(UnloadFont) { font: { baseSize: 1 glyphCount: 2 glyphPadding: 3 } }"
-        ]
-      end
-
       def test_initialize_with_args
         Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 6, glyph_count: 100, glyph_padding: 4))
 
@@ -54,6 +42,17 @@ class Test
 
         assert_called [
           "(FileExists) { fileName: './assets/fail.ttf' }"
+        ]
+      end
+
+      def test_unload
+        Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 1, glyph_count: 2, glyph_padding: 3))
+        font = Font.new("./assets/tiny.ttf")
+        Taylor::Raylib.reset_calls
+
+        font.unload
+        assert_called [
+          "(UnloadFont) { font: { baseSize: 1 glyphCount: 2 glyphPadding: 3 } }"
         ]
       end
 
@@ -84,12 +83,17 @@ class Test
 
       def test_ready?
         Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 4, glyph_count: 5, glyph_padding: 6))
-
         font = Font.new("./assets/tiny.ttf")
         Taylor::Raylib.reset_calls
 
-        font.ready?
+        Taylor::Raylib.mock_call("IsFontReady", "true")
+        Taylor::Raylib.mock_call("IsFontReady", "false")
+
+        assert_true font.ready?
+        assert_false font.ready?
+
         assert_called [
+          "(IsFontReady) { font: { baseSize: 4 glyphCount: 5 glyphPadding: 6 } }",
           "(IsFontReady) { font: { baseSize: 4 glyphCount: 5 glyphPadding: 6 } }"
         ]
       end
@@ -106,176 +110,116 @@ class Test
         ]
       end
 
-      def test_draw_with_size
+      def test_draw_with_args
         Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 6))
-
         font = Font.new("./assets/tiny.ttf", size: 6)
         Taylor::Raylib.reset_calls
 
-        font.draw("x", size: 12)
+        font.draw(
+          "x",
+          size: 12,
+          spacing: 13,
+          colour: Colour[1, 2, 3, 4]
+        )
 
         assert_called [
-          "(DrawTextEx) { font: { baseSize: 6 glyphCount: 0 glyphPadding: 0 } text: 'x' position: { x: 0.000000 y: 0.000000 } fontSize: 12.000000 spacing: 0.000000 tint: { r: 0 g: 0 b: 0 a: 255 } }"
+          "(DrawTextEx) { font: { baseSize: 6 glyphCount: 0 glyphPadding: 0 } text: 'x' position: { x: 0.000000 y: 0.000000 } fontSize: 12.000000 spacing: 13.000000 tint: { r: 1 g: 2 b: 3 a: 4 } }"
         ]
       end
 
-      def test_draw_with_position
-        Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 6))
-
-        font = Font.new("./assets/tiny.ttf", size: 6)
+      def test_draw_with_vector
+        Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 8))
+        font = Font.new("./assets/tiny.ttf", size: 8)
         Taylor::Raylib.reset_calls
 
         font.draw(
           "q",
-          position: Vector2.new(2, 2)
+          position: Vector2.new(2, 3)
         )
 
         assert_called [
-          "(DrawTextEx) { font: { baseSize: 6 glyphCount: 0 glyphPadding: 0 } text: 'q' position: { x: 2.000000 y: 2.000000 } fontSize: 6.000000 spacing: 0.000000 tint: { r: 0 g: 0 b: 0 a: 255 } }"
+          "(DrawTextEx) { font: { baseSize: 8 glyphCount: 0 glyphPadding: 0 } text: 'q' position: { x: 2.000000 y: 3.000000 } fontSize: 8.000000 spacing: 0.000000 tint: { r: 0 g: 0 b: 0 a: 255 } }"
         ]
       end
 
       def test_draw_with_x_and_y
-        Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 6))
-
-        font = Font.new("./assets/tiny.ttf", size: 6)
+        Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 9))
+        font = Font.new("./assets/tiny.ttf", size: 9)
         Taylor::Raylib.reset_calls
 
         font.draw(
           "y",
-          x: 2, y: 0
+          x: 2, y: 1
         )
 
         assert_called [
-          "(DrawTextEx) { font: { baseSize: 6 glyphCount: 0 glyphPadding: 0 } text: 'y' position: { x: 2.000000 y: 0.000000 } fontSize: 6.000000 spacing: 0.000000 tint: { r: 0 g: 0 b: 0 a: 255 } }"
+          "(DrawTextEx) { font: { baseSize: 9 glyphCount: 0 glyphPadding: 0 } text: 'y' position: { x: 2.000000 y: 1.000000 } fontSize: 9.000000 spacing: 0.000000 tint: { r: 0 g: 0 b: 0 a: 255 } }"
         ]
       end
 
-      # def test_draw_with_colour
-      #  skip_unless_display_present
+      def test_measure
+        Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 9))
+        font = Font.new("./assets/tiny.ttf", size: 9)
+        Taylor::Raylib.reset_calls
+        Taylor::Raylib.mock_call("MeasureTextEx", "1 2")
 
-      #  set_window_title(__method__.to_s)
-      #  font = Font.new("./assets/tiny.ttf", size: 6)
-      #  clear_and_draw do
-      #    font.draw(
-      #      "O",
-      #      colour: Colour::GREEN
-      #    )
-      #  end
+        assert_equal Vector2[1, 2], font.measure("xx")
 
-      #  assert_equal fixture_font_draw_with_colour, get_screen_data.data
-      # ensure
-      #  font.unload
-      # end
+        assert_called [
+          "(MeasureTextEx) { font: { baseSize: 9 glyphCount: 0 glyphPadding: 0 } text: 'xx' fontSize: 9.000000 spacing: 0.000000 }"
+        ]
+      end
 
-      # def test_unload
-      #  font = Font.new("./assets/tiny.ttf")
-      #  assert_true font.ready?
-      # ensure
-      #  font.unload
-      # end
+      def test_measure_with_args
+        Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 10))
+        font = Font.new("./assets/tiny.ttf", size: 10)
+        Taylor::Raylib.reset_calls
+        Taylor::Raylib.mock_call("MeasureTextEx", "2 3")
 
-      # def test_draw_with_spacing
-      #  skip_unless_display_present
+        assert_equal Vector2[2, 3], font.measure("qq", size: 11, spacing: 12)
 
-      #  set_window_title(__method__.to_s)
-      #  font = Font.new("./assets/tiny.ttf", size: 6)
-      #  clear_and_draw do
-      #    font.draw(
-      #      "xx",
-      #      spacing: 1
-      #    )
-      #  end
+        assert_called [
+          "(MeasureTextEx) { font: { baseSize: 10 glyphCount: 0 glyphPadding: 0 } text: 'qq' fontSize: 11.000000 spacing: 12.000000 }"
+        ]
+      end
 
-      #  assert_equal fixture_font_draw_with_spacing, get_screen_data.data
-      # ensure
-      #  font.unload
-      # end
+      def test_to_image
+        Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 11))
+        font = Font.new("./assets/tiny.ttf", size: 11)
+        Taylor::Raylib.reset_calls
+        Taylor::Raylib.mock_call("ImageTextEx", Image.mock_return(width: 1, height: 2, mipmaps: 3, format: 4))
 
-      # def test_measure
-      #  skip_unless_display_present
+        image = font.to_image("xx")
 
-      #  set_window_title(__method__.to_s)
-      #  font = Font.new("./assets/tiny.ttf", size: 6)
-      #  assert_equal 8, font.measure("xx").width
-      #  assert_equal 6, font.measure("xx").height
-      # ensure
-      #  font.unload
-      # end
+        assert_kind_of Image, image
+        assert_equal 1, image.width
+        assert_equal 2, image.height
+        assert_equal 3, image.mipmaps
+        assert_equal 4, image.format
 
-      # def test_measure_with_size
-      #  skip_unless_display_present
+        assert_called [
+          "(ImageTextEx) { font: { baseSize: 11 glyphCount: 0 glyphPadding: 0 } text: 'xx' fontSize: 11.000000 spacing: 0.000000 tint: { r: 0 g: 0 b: 0 a: 255 } }"
+        ]
+      end
 
-      #  set_window_title(__method__.to_s)
-      #  font = Font.new("./assets/tiny.ttf", size: 6)
-      #  assert_equal 16, font.measure("xx", size: 12).width
-      #  assert_equal 12, font.measure("xx", size: 12).height
-      # ensure
-      #  font.unload
-      # end
+      def test_to_image_with_args
+        Taylor::Raylib.mock_call("LoadFontEx", Font.mock_return(size: 12))
+        font = Font.new("./assets/tiny.ttf", size: 12)
+        Taylor::Raylib.reset_calls
+        Taylor::Raylib.mock_call("ImageTextEx", Image.mock_return(width: 2, height: 3, mipmaps: 4, format: 5))
 
-      # def test_measure_with_spacing
-      #  skip_unless_display_present
+        image = font.to_image("x", size: 13, spacing: 14, colour: Colour[2, 3, 4, 5])
 
-      #  set_window_title(__method__.to_s)
-      #  font = Font.new("./assets/tiny.ttf", size: 6)
-      #  assert_equal 9, font.measure("xx", spacing: 1).width
-      #  assert_equal 6, font.measure("xx", spacing: 1).height
-      # ensure
-      #  font.unload
-      # end
+        assert_kind_of Image, image
+        assert_equal 2, image.width
+        assert_equal 3, image.height
+        assert_equal 4, image.mipmaps
+        assert_equal 5, image.format
 
-      # def test_to_image
-      #  skip_unless_display_present
-
-      #  set_window_title(__method__.to_s)
-      #  font = Font.new("./assets/tiny.ttf", size: 6)
-      #  image = font.to_image("xx")
-      #  assert_equal fixture_font_to_image, image.data
-      # ensure
-      #  font.unload
-      #  image.unload
-      # end
-
-      # def test_to_image_with_size
-      #  skip_unless_display_present
-
-      #  set_window_title(__method__.to_s)
-      #  font = Font.new("./assets/tiny.ttf", size: 6)
-      #  image = font.to_image("x", size: 12, colour: Colour::GREEN)
-      #  # I suspect there's a bug here in Raylib where there is a filter being
-      #  # applied somewhere in the process. Because just drawing it to the
-      #  # screen at an exact integer scale works fine, but here it's slightly
-      #  # blurry.
-      #  assert_equal fixture_font_to_image_with_size, image.data
-      # ensure
-      #  font.unload
-      #  image.unload
-      # end
-
-      # def test_to_image_with_spacing
-      #  skip_unless_display_present
-
-      #  set_window_title(__method__.to_s)
-      #  font = Font.new("./assets/tiny.ttf", size: 6)
-      #  image = font.to_image("xx", spacing: 1)
-      #  assert_equal fixture_font_to_image_with_spacing, image.data
-      # ensure
-      #  font.unload
-      #  image.unload
-      # end
-
-      # def test_to_image_with_colour
-      #  skip_unless_display_present
-
-      #  set_window_title(__method__.to_s)
-      #  font = Font.new("./assets/tiny.ttf", size: 6)
-      #  image = font.to_image("xx", colour: Colour::GREEN)
-      #  assert_equal fixture_font_to_image_with_colour, image.data
-      # ensure
-      #  font.unload
-      #  image.unload
-      # end
+        assert_called [
+          "(ImageTextEx) { font: { baseSize: 12 glyphCount: 0 glyphPadding: 0 } text: 'x' fontSize: 13.000000 spacing: 14.000000 tint: { r: 2 g: 3 b: 4 a: 5 } }"
+        ]
+      end
     end
   end
 end
