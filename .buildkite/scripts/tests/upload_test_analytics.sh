ed7840e769e8d0b960bf35d5b1541381ff26ec59
Sean Earle <sean.r.earle@gmail.com>
2025-08-16 15:44:53 +0930

    Don't try to upload a file that doesn't exist for buildkite analytics

--- .buildkite/scripts/tests/upload_test_analytics.sh
+++ .buildkite/scripts/tests/upload_test_analytics.sh
@@ -6,17 +6,20 @@ if ! [[ -v BUILDKITE_TEST_ANALYTICS_KEY ]]; then
   exit 0
 fi
 
-curl \
-  -X POST \
-  -H "Authorization: Token token=\"$BUILDKITE_TEST_ANALYTICS_KEY\"" \
-  -F "data=@$1" \
-  -F "format=json" \
-  -F "run_env[CI]=buildkite" \
-  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
-  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
-  -F "run_env[branch]=$BUILDKITE_BRANCH" \
-  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
-  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
-  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
-  -F "run_env[message]=$BUILDKITE_MESSAGE" \
-  https://analytics-api.buildkite.com/v1/uploads
+if [[ -f $1 ]]; then
+  echo "Analytics file found, upload '$1'"
+  curl \
+    -X POST \
+    -H "Authorization: Token token=\"$BUILDKITE_TEST_ANALYTICS_KEY\"" \
+    -F "data=@$1" \
+    -F "format=json" \
+    -F "run_env[CI]=buildkite" \
+    -F "run_env[key]=$BUILDKITE_BUILD_ID" \
+    -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+    -F "run_env[branch]=$BUILDKITE_BRANCH" \
+    -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
+    -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+    -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
+    -F "run_env[message]=$BUILDKITE_MESSAGE" \
+    https://analytics-api.buildkite.com/v1/uploads
+fi
