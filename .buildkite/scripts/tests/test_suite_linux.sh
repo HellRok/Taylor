c80ff005fc03a1b544c9b368c5a863757a75c8d3
Sean Earle <sean.r.earle@gmail.com>
2025-08-21 20:44:37 +0930

    Fix broken build

--- .buildkite/scripts/tests/test_suite_linux.sh
+++ .buildkite/scripts/tests/test_suite_linux.sh
@@ -8,6 +8,5 @@ bundle install
 mkdir -p ./tmp
 buildkite-agent artifact download taylor-test-suite-linux-v0.0.1.zip ./tmp
 pushd ./tmp
-cd /tmp/buildkite/taylor-test-suite/linux/
 unzip taylor-test-suite-linux-v0.0.1.zip
 ./taylor-test-suite
