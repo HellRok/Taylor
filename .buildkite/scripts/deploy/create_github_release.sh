5fdeeea2c6495b45e8cc09a44b7de80e729a56dd
Sean Earle <sean.r.earle@gmail.com>
2025-10-12 19:45:25 +1030

    Fix the deploy pipeline

--- .buildkite/scripts/deploy/create_github_release.sh
+++ .buildkite/scripts/deploy/create_github_release.sh
@@ -2,12 +2,16 @@
 set -euo pipefail
 
 echo $GITHUB_AUTH_TOKEN | gh auth login --with-token
-cp -r cli-tool /tmp/buildkite/
-pushd /tmp/buildkite/cli-tool
+
+BUILD_PATH=$(pwd)
+TEMP_DIR=$(mktemp --dir /tmp/buildkite/taylor.XXXXXXXXXXXX)
+
+cp -r cli-tool "${TEMP_DIR}"
+pushd "${TEMP_DIR}/cli-tool"
 taylor ./cli.rb export
 popd
 
 gh release create \
   --notes-file release-body.md \
   --title "Release $BUILDKITE_BRANCH" \
-  $BUILDKITE_BRANCH /tmp/buildkite/cli-tool/exports/*.zip
+  $BUILDKITE_BRANCH "${TEMP_DIR}/cli-tool/exports/*.zip"
